3d715389d23054c76d43876bbf052e52b011dbca
DementedPlant <dementedplant@gmail.com>
2022-06-10 09:39:26 +0100

    færdiggjorde koden

--- script.js
+++ script.js
@@ -2,16 +2,16 @@
 
 function openprogram(evt, programName) {
 	
-  //Vi deklarerer 3 var variabler; i, tabcontent og tablinks
-  var i, tabcontent, tablinks;
+  //Vi deklarerer 3 var variabler; i, menuIndhold og tablinks
+  var i, menuIndhold, tablinks;
 	
-  //Her indsætter vi variablen tabcontents værdi som værene document.getElementsByClassName("tabcontent")
-  tabcontent = document.getElementsByClassName("tabcontent");
+  //Her indsætter vi variablen menuIndholds værdi som værene document.getElementsByClassName("menuIndhold")
+  menuIndhold = document.getElementsByClassName("menuIndhold");
 	
   //Sørger for at teksten i tekstboksen bliver erstattet af den nye tekst når en knap bliver trykket, i steddet for at blive skrevet under, hvilket resulterer i en større og større tekstboks
   //For loopet sørger for at koden indenfor dets curly brackets kun kører en gang
-  for (i = 0; i < tabcontent.length; i++) {
-  tabcontent[i].style.display = "none";
+  for (i = 0; i < menuIndhold.length; i++) {
+  menuIndhold[i].style.display = "none";
   }
 
   //Sørger for at teksten dukker op i tekstboksen når funktionen bliver kaldt, når man trykker på en knap
